97190b6d48a02d2a174b968639a09383fd17f640
Alfredo Lima Silva <alfredo.silva@epe.gov.br>
2024-05-29 23:28:19 -0300

    Agora sim!! Ínicio do dashboard. Primeiras tabs

--- R/app.R
+++ R/app.R
@@ -1,19 +1,52 @@
 library(shiny)
 library(purrr)
+library(shinydashboard)
+library(DT)
 
-myapp <- function(...){
 
-  ui <- fluidPage(title = 'Investimentos',
-    fileInput('dados_brutos', 'Selecione os arquivos de negociacao', accept = '.xlsx', multiple = TRUE),
-    DT::DTOutput('negociacao')
+
+  ui <- dashboardPage(
+
+    dashboardHeader(title = 'Investimentos'),
+
+    dashboardSidebar(
+      sidebarMenu(
+        menuItem('Negociações', tabName = 'negociacao'),
+        menuItem('Ações', tabName = 'df_acao')
+      )
+    ),
+
+    dashboardBody(
+      tabItems(
+        tabItem(tabName = 'negociacao', 'Negociações registradas na B3',
+                fluidRow(
+                  box(width = 3,
+                      fileInput(inputId = 'dado_bruto', label = 'Selecione os arquivos de Negociação',
+                                multiple = TRUE, accept = '.xlsx')
+                      ),
+                  box(width = 9, title = 'Arquivos Selecionados',
+                      textOutput(outputId = 'arquivos'))
+                  ),
+
+                fluidRow(
+                  box(width = 12,
+                    DT::DTOutput(outputId = 'df_dado_bruto')
+                  )
+                )
+                ),
+        tabItem(tabName = 'df_acao', 'Negociação de Ações')
+        )
+      )
+
   )
 
   server <- function(input, output, session) {
 
+
     dados <- reactive({
 
-      req(input$dados_brutos)
-      df <- map(input$dados_brutos$datapath, .f = carrega_dados) %>%
+      req(input$dado_bruto)
+      df <- map(input$dado_bruto$datapath, .f = carrega_dados) %>%
         bind_rows() %>%
         arrange(data_negocio) %>%
         mutate(data_negocio = format(as.Date(data_negocio), "%d/%m/%Y")) %>%
@@ -21,13 +54,18 @@ myapp <- function(...){
 
     })
 
-    output$negociacao <- DT::renderDT({
+    output$df_dado_bruto <- DT::renderDT({
 
       dados()
 
     })
 
+
   }
   shinyApp(ui, server)
-}
+
+
+
+
+
 
